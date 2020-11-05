fea6100f1d340c5e32e1e89fb8d5ec4e0c55f475
Aggressive Combustion Game Studios <48961783+AggressiveCombustion@users.noreply.github.com>
2020-11-04 18:13:32 -0600

    Hello World

---
+++ ThirdPerson/Assets/EnemyController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Movement Variables")]
+    [SerializeField]
+    float moveSpeed = 1.0f;
+    [SerializeField]
+    float meleeRange = 1.0f;
+
+    public LayerMask playerLayer;
+    public LayerMask environmentLayer;
+
+    [Header("References")]
+    [SerializeField]
+    PlayerController player;
+    [SerializeField]
+    EnemySight sight;
+    [SerializeField]
+    Collider[] ragdollColliders;
+
+    Vector3 goalLocation;
+    bool shouldMove = false;
+    bool dead = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (Collider c in ragdollColliders)
+        {
+            c.isTrigger = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dead)
+            return;
+        if(ScanForPlayer())
+        {
+            shouldMove = true;
+            transform.LookAt(new Vector3(goalLocation.x, 0, goalLocation.z));
+        }
+
+        if(shouldMove && Vector3.Distance(transform.position, goalLocation) < meleeRange)
+        {
+            shouldMove = false;
+        }
+        
+        GetComponent<Rigidbody>().velocity = transform.forward * moveSpeed * (shouldMove ? 1 : 0);
+        GetComponent<Animator>().SetFloat("moveSpeed", GetComponent<Rigidbody>().velocity.magnitude);
+
+        /*if(Input.GetButton("Jump"))
+        {
+            GoRagdoll();
+        }*/
+    }
+
+    bool ScanForPlayer()
+    {
+        foreach (SightInformation s in sight.CheckForObject())
+        {
+            // if we find player, and we're not super close, set goal location
+            if (s.seen && s.tag == "Player" && Vector3.Distance(transform.position, s.location) > meleeRange)
+            {
+                goalLocation = s.location;
+                goalLocation.y = 0;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Sword" && !dead)
+        {
+            GoRagdoll();
+
+            Destroy(gameObject, 3.0f);
+        }
+    }
+
+    void GoRagdoll()
+    {
+        shouldMove = false;
+        GetComponent<CapsuleCollider>().isTrigger = true;
+        dead = true;
+        GetComponent<Animator>().enabled = false;
+
+        foreach (Collider c in ragdollColliders)
+        {
+            c.isTrigger = false;
+        }
+    }
+
+    
+}
---
+++ ThirdPerson/Assets/EnemySight.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+    [SerializeField]
+    float degreesOfSight = 360.0f;
+    [SerializeField]
+    float sightDistance = 20.0f;
+    [SerializeField]
+    int divisions = 180;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 LookForThing(LayerMask layer)
+    {
+        Vector3 validPoint = Vector3.zero;
+
+        for (int i = 0; i < divisions; i++)
+        {
+            float value = i * (degreesOfSight / divisions);
+
+            Vector3 forward = transform.forward;
+
+            Vector3 rotated = Quaternion.AngleAxis(value, Vector3.up) * transform.forward;
+
+            Vector3 endPoint = transform.position + (rotated * sightDistance);
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, (rotated * sightDistance), out hit, sightDistance, layer))
+            {
+                // we hit a thing that qualifies
+                endPoint = hit.point;
+
+                if (validPoint == Vector3.zero)
+                    validPoint = endPoint;
+            }
+
+            Debug.DrawLine(transform.position, endPoint, Color.red);
+        }
+
+        return validPoint;
+    }
+
+    public SightInformation[] CheckForObject()
+    {
+        List<SightInformation> sights = new List<SightInformation>();
+        for (int i = 0; i < divisions; i++)
+        {
+            float value = i * (degreesOfSight / divisions);
+
+            Vector3 forward = transform.forward;
+
+            Vector3 rotated = Quaternion.AngleAxis(value, Vector3.up) * transform.forward;
+
+            Vector3 endPoint = transform.position + (rotated * sightDistance);
+
+            RaycastHit hit;
+
+            SightInformation s = new SightInformation();
+
+            if (Physics.Raycast(transform.position, (rotated * sightDistance), out hit, sightDistance))
+            {
+                // we hit a thing that qualifies
+                endPoint = hit.point;
+                s.tag = hit.transform.tag;
+                s.seen = true;
+            }
+
+            Debug.DrawLine(transform.position, endPoint, Color.red);
+
+            s.location = endPoint;
+            sights.Add(s);
+        }
+
+        return sights.ToArray();
+    }
+}
+
+public class SightInformation
+{
+    public Vector3 location;
+    public bool seen;
+    public string tag;
+}
